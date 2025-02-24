fcf7966644eebf3ceb6dbe3ff863e9d638bff16f
Moheuddin Monna <moheuddin.monnab4u@gmail.com>
2025-02-25 07:19:17 +0800

    after a update the text , the cards alignment was not nice in mobile view. did change some margin padding , now the cards looks nice

--- app/component/AutomationLifestyle.jsx
+++ app/component/AutomationLifestyle.jsx
@@ -5,32 +5,32 @@ function AutomationLifestyle() {
 
   const data = [
     {
-      "img": "/automationLifeImages/img1.jpg", 
+      "img": "/automationLifeImages/img1.jpg",
       "name": "Free to Choose the Best",
-      "disc": "Expand your possibilities with unlimited device compatibility. Whether it’s a Dyson, Tesla car, Samsung washer, Aqara, Yale lock, or Google Assistant—choose the brands you love and integrate seamlessly with AutoLife."   
+      "disc": "Expand your possibilities with unlimited device compatibility. Whether it’s a Dyson, Tesla car, Samsung washer, Aqara, Yale lock, or Google Assistant—choose the brands you love and integrate seamlessly with AutoLife."
     },
     {
-      "img": "/automationLifeImages/img2.jpg", 
+      "img": "/automationLifeImages/img2.jpg",
       "name": "Enhance Home Security",
       "disc": "With over 4,600+ homes and offices secured, we transform your space into a safe sanctuary. Cherish precious moments with your family, knowing your home is protected"
     },
     {
-      "img": "/automationLifeImages/img3.png", 
+      "img": "/automationLifeImages/img3.png",
       "name": "Maintain Healthy Homes",
       "disc": "Breathe easy with safe air quality, integrated smoke detection, automated robot vacuums, leak prevention to combat mold, and smart toilet ventilation. We create healthier living environments effortlessly"
     },
     {
-      "img": "/automationLifeImages/img4.jpg", 
+      "img": "/automationLifeImages/img4.jpg",
       "name": "Operate Without Internet, Reliability",
       "disc": "Boost reliability and uptime by keeping your home’s automation independent of external internet connections. Your data stays within your home—secure, private, and efficient."
     },
     {
-      "img": "/automationLifeImages/img5.jpg", 
+      "img": "/automationLifeImages/img5.jpg",
       "name": "Experience Remote Access",
       "disc": "Stay connected to your home wherever you are. Whether on holiday or at work, get real-time insights and control everything at a glance through your phone."
     },
     {
-      "img": "/automationLifeImages/img6.gif", 
+      "img": "/automationLifeImages/img6.gif",
       "name": "Secured Cloud Back Up",
       "disc": "Enhance reliability with routine backups of your home data and custom configurations. Rest assured—our dedicated customer support is always ready to assist when you need it most."
     }
@@ -39,41 +39,39 @@ function AutomationLifestyle() {
   // const [datas, setDatas] = useState(data);
 
   return (
-     // md:px-20
-    <div className="mx-auto py-14 px-4 sm:px-6  lg:px-32 min-h-96 mb-4 w-full overflow-hidden relative"> 
+    // md:px-20
+    <div className="mx-auto py-14 px-4 sm:px-6  lg:px-32 min-h-96 mb-4 w-full overflow-hidden relative">
 
-      <div className="text-center mb-12">
-        <p className="text-gray-600 text-lg font-bold sm:text-3xl md:text-4xl mb-4 md:w-3/4 w-full mx-auto">
-          A truly thoughtful home automation considers the lifestyle you want
-        </p>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-        {
-          data.map((e, i) => (
-            <div className="relative flex-shrink-0 w-full" key={i}>
+    <div className="text-center mb-12">
+      <p className="text-gray-600 text-lg font-bold sm:text-3xl md:text-4xl mb-4 md:w-3/4 w-full mx-auto">
+        A truly thoughtful home automation considers the lifestyle you want
+      </p>
+    </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+      {
+        data.map((e, i) => (
+          <div className="relative flex-shrink-0 w-full" key={i}>
 
-              <img src={e.img} alt={`Automation Life ${i + 1}`} className="w-full h-64 object-cover mb-14  rounded-lg shadow-lg" />
+            <img src={e.img} alt={`Automation Life ${i + 1}`} className="w-full h-64 object-cover mb-14  rounded-lg shadow-lg" />
 
-              <div className="absolute bottom-1 left-0 right-0 flex justify-center ">
+            <div className="absolute bottom-1 left-0 right-0 flex justify-center ">
 
-                <div className="inline-block bg-white p-6 sm:p-3  shadow-lg">
-                  <h1 className="lg:text-lg sm:text-xl font-semibold text-center">{e.name}</h1>
-                  <p className=" lg:text-xs sm:text-xs text-gray-700 text-center mt-2">
-                    {e.disc}
-                  </p>
-                </div>
-
+              <div className="inline-block bg-white p-6 sm:p-4  shadow-lg">
+                <h1 className="text-lg sm:text-xl font-semibold text-center">{e.name}</h1>
+                <p className=" text-xs sm:text-xs text-gray-700 text-center mt-2">
+                  {e.disc}
+                </p>
               </div>
 
             </div>
-          ))
-        }
 
-      </div>
+          </div>
+        ))
+      }
 
     </div>
 
-
+  </div>
   );
 }
 
